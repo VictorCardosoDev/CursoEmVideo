54e40a1c0173792e42a3b98567d462a637e2c5d6
victorhenriq <victor_henriquesc@outlook.com>
2022-10-27 16:51:44 -0300

    Aprendendo sobre condições

---
+++ javascript/aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 12;
+console.log(`A velocidade do seu carro é ${vel}KM/h`);
+
+if (vel > 60) {
+    console.log('\nA sua velocidade é maior que a permitida! Multado!')
+}
+
+console.log('\nDirija sempre usando cinto de segurança!');
