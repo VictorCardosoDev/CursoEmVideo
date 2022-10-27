d2aa3c8a70684e53bedc71d92fc8cc8e98e0e744
victorhenriq <victor_henriquesc@outlook.com>
2022-10-27 17:53:45 -0300

    Aprendendo condições aninhadas

---
+++ javascript/aula12/ex014.js
@@ -0,0 +1,20 @@
+//condição aninhada;
+
+var agr = new Date();
+
+var hora = agr.getHours();
+console.log(`Agora são exatamente ${hora} horas`);
+
+if (hora < 6 && hora > 0 || hora == 24) {
+    console.log('Boa madrugada!');
+} else if (hora < 12) {
+    console.log('Bom dia!');
+
+} else if (hora < 18) {
+    console.log('Boa tarde!');
+
+} else if (hora > 18 && hora <= 23) {
+    console.log('Boa noite!');
+} else {
+    console.log('Esse horário não é válido!');
+}
