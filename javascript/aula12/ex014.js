4127990d87ebf60498d4f1c74ed457ffb22eb53b
victorhenriq <victor_henriquesc@outlook.com>
2022-10-27 21:05:49 -0300

    Saudações que mudam de acordo com o turno do dia

--- javascript/aula12/ex014.js
+++ javascript/aula12/ex014.js
@@ -5,7 +5,7 @@ var agr = new Date();
 var hora = agr.getHours();
 console.log(`Agora são exatamente ${hora} horas`);
 
-if (hora < 6 && hora > 0 || hora == 24) {
+if (hora < 6 && hora > 0) {
     console.log('Boa madrugada!');
 } else if (hora < 12) {
     console.log('Bom dia!');
@@ -13,7 +13,7 @@ if (hora < 6 && hora > 0 || hora == 24) {
 } else if (hora < 18) {
     console.log('Boa tarde!');
 
-} else if (hora > 18 && hora <= 23) {
+} else if (hora > 18 && hora <= 24) {
     console.log('Boa noite!');
 } else {
     console.log('Esse horário não é válido!');
