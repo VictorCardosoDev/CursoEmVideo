6a48471c609e1ff75ed01f71866d3db8b6da7b97
victorhenriq <victor_henriquesc@outlook.com>
2022-10-31 22:07:32 -0300

    estrutura de repetição while e do-while

---
+++ javascript/aula13/ambiente.js
@@ -0,0 +1,25 @@
+var c = 1;
+
+do {
+    console.log(`${c}: Tudo bem ?`);
+    c++;
+} while (c <= 6);
+
+
+console.log('\n');
+
+var c = 1;
+
+while (c <= 6) {
+    console.log(`${c}: Tudo bem ?`);
+    c++;
+}
+
+console.log('\n');
+ 
+console.log("1: Tudo bem ?");
+console.log("2: Tudo bem ?");
+console.log("3: Tudo bem ?");
+console.log("4: Tudo bem ?");
+console.log("5: Tudo bem ?");
+console.log("6: Tudo bem ?");
